84e94c46466042da1f6d54d3fded81946c818eb6
omerdikici <omer.dikici@tedu.edu.tr>
2025-02-25 15:55:51 +0300

    okey

--- demo.c
+++ demo.c
@@ -4,3 +4,32 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+//int main() {
+  //  int num;
+
+    // Kullanıcıdan bir sayı al
+   // printf("Bir sayı girin: ");
+  //  scanf("%d", &num);
+
+    // Sayının çift mi tek mi olduğunu kontrol et
+   //// if (num % 2 == 0) {
+    //    printf("%d bir çift sayıdır.\n", num);
+   // } else {
+    //    printf("%d bir tek sayıdır.\n", num);
+    //}
+
+    //return 0;
+//}
+
+//#include <stdio.h>
+
+//int main() {
+ //   printf("Önceki çıktılar burada olabilir...\n");
+
+    // Terminali temizle
+  //  printf("\033[H\033[J");
+
+   // printf("Ekran temizlendi!\n");
+
+  //  return 0;
+//}
